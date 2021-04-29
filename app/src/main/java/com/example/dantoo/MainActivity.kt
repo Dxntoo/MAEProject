be46c0ed9eeeecd11067053cd90a13428b3342db
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-04-30 01:41:30 +0800

    initialized files

---
+++ app/src/main/java/com/example/dantoo/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.dantoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.dantoo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailTextField.text.toString()
+            val password = binding.passwordTextField.text.toString()
+
+            Log.d("Email is: ", email)
+            Log.d("Password is: ", password)
+        }
+
+
+
+        
+    }
+
+
+
+}
