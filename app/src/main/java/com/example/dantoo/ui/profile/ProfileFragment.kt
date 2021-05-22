0fbe0c6eac5a1e9b9e386efd3bbb9b887ca8a3bf
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-05-22 17:52:25 +0800

    Added splash screen

---
+++ app/src/main/java/com/example/dantoo/ui/profile/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.example.dantoo.ui.profile
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dantoo.R
+import com.example.dantoo.databinding.FragmentProfileBinding
+import com.example.dantoo.ui.profile.ProfileViewModel
+
+class ProfileFragment : Fragment() {
+    private lateinit var profileViewModel: ProfileViewModel
+    private var _binding: FragmentProfileBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
