eaa8dd699ca1a16ca0f820adecd99847f4c75a0e
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-05-24 16:52:22 +0800

    signingreport error

--- app/src/main/java/com/example/dantoo/RegisterActivity.kt
+++ app/src/main/java/com/example/dantoo/RegisterActivity.kt
@@ -2,11 +2,21 @@ package com.example.dantoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dantoo.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+    }
+
+    private fun registerAccount(){
+
 
     }
 }
